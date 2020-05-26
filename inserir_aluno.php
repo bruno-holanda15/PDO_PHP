8bbf43e3de42fb7eef4823c1b5a3d6a539bd7b6b
Bruno Holanda <br.holanda15@gmail.com>
2020-05-26 18:02:27 -0300

    Preparando a query de Insert , utilizando prepare() e ->bindValue()

--- inserir_aluno.php
+++ inserir_aluno.php
@@ -7,7 +7,20 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . "/banco.sqlite";
 $pdo = new PDO("sqlite:" . $databasePath);
 
-$student = new Student(null, 'Bruno Holanda', new \DateTimeImmutable('1995-03-15'));
+$student = new Student(
+    null,
+    "Marcelo Holanda', ''); DROP TABLE students;",
+    new \DateTimeImmutable('1967-11-24')
+);
 
-$sqlInsert = "INSERT INTO students(name,birth_date) VALUES('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}')";
-var_dump($pdo->exec($sqlInsert));
+$sqlInsert = "INSERT INTO students(name,birth_date) VALUES(:nome, :data_nasc);";
+
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(":nome", $student->name());
+$statement->bindValue(":data_nasc", $student->birthDate()->format('Y-m-d'));
+
+if($statement->execute()){
+    echo "aluno incluido";
+}else{
+    echo "aluno nao incluido";
+}
