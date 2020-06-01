4961acd09f12eb111c658b1eaa0a19f01d9de3dc
Bruno Holanda <br.holanda15@gmail.com>
2020-06-01 16:38:51 -0300

    Criando tabela e model para Phones

--- conexao.php
+++ conexao.php
@@ -5,10 +5,21 @@ $databasePath = __DIR__ . "/banco.sqlite";
 // PDO é uma Interface para podermos acessar diferentes tipos de banco de dados, cada um com seu driver especifico
 $pdo = new PDO("sqlite:" . $databasePath);
 
-echo "Conectei";
+echo "Conectei" . PHP_EOL;
 
-$pdo->exec('CREATE TABLE students (
-                id INTEGER PRIMARY KEY,
-                name TEXT,
-                birth_date TEXT            
-)');
+$createSql = ' CREATE TABLE IF NOT exists students (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT,
+                    birth_date TEXT
+                );
+
+                CREATE TABLE IF NOT exists phones (
+                    id INTEGER PRIMARY KEY,
+                    area_code TEXT,
+                    number TEXT,
+                    student_id INTEGER,
+                    FOREIGN KEY(student_id) REFERENCES students(id)
+                );
+';
+
+$pdo->exec($createSql);
